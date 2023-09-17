e300bbb8a62f65a0964d1f5bd3cce6abfbb53940
Walter Sosa <wasosa@yahoo.com>
2023-09-16 22:51:02 -0700

    Auto update! Plus formatting improvements.
    Update every 100ms!
    Added seconds; TODO: make this optional
    Use 12-hour format; TODO: make this optional

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Trivial clock page
 
+function pad(value)
+{
+    if (value < 10) {
+        return "0" + value.toString();
+    } else {
+        return value.toString();
+    }
+}
+
+function getTime(date)
+{
+    let hours = date.getHours();
+    let period = "AM";
+    if (hours > 12) {
+        hours = hours - 12;
+        period = "PM";
+    }
+    hours = pad(hours);
+    let mins = pad(date.getMinutes());
+    let secs = pad(date.getSeconds());
+    return hours + ":" + mins + ":" + secs + " " + period;
+}
+
 let current_time = document.getElementById("current-time");
-let now = new Date();
-current_time.innerHTML = now.getHours() + ":" + now.getMinutes();
+setInterval(
+    () => {
+        current_time.innerHTML = getTime(new Date());
+    },
+    100
+);
