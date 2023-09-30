6b7466b57c341187629d2c36902eda64f43081e8
Walter Sosa <wasosa@yahoo.com>
2023-09-29 19:41:50 -0700

    Make it optional to show seconds

--- script.js
+++ script.js
@@ -34,25 +34,33 @@ function parseTime(date)
     }
 }
 
-function formatTime12(time)
+function formatTime12(time, seconds)
 {
-    return time.hours12 + ":" + time.minutes + ":" + time.seconds + " " + time.period;
+    if (seconds == "true") {
+        return time.hours12 + ":" + time.minutes + ":" + time.seconds + " " + time.period;
+    } else {
+        return time.hours12 + ":" + time.minutes + " " + time.period;
+    }
 }
 
-function formatTime24(time)
+function formatTime24(time, seconds)
 {
-    return time.hours24 + ":" + time.minutes + ":" + time.seconds;
+    if (seconds == "true") {
+        return time.hours24 + ":" + time.minutes + ":" + time.seconds;
+    } else {
+        return time.hours24 + ":" + time.minutes;
+    }
 }
 
 function formatTime(time, format)
 {
     if (format["hours"] == 12) {
-        return formatTime12(time)
+        return formatTime12(time, format["seconds"])
     } else if (format["hours"] == 24) {
-        return formatTime24(time)
+        return formatTime24(time, format["seconds"])
     } else {
         console.error("Unknown time format; defaulting to 12-hour format")
-        return formatTime12(time)
+        return formatTime12(time, format["seconds"])
     }
 }
 
@@ -66,9 +74,10 @@ function main()
 {
     let current_time = document.getElementById("current-time");
     let hours = document.currentScript.getAttribute("hours");
+    let seconds = document.currentScript.getAttribute("seconds");
     setInterval(
         () => {
-            current_time.innerHTML = getTime(new Date(), { hours: hours });
+            current_time.innerHTML = getTime(new Date(), { hours: hours, seconds: seconds });
         },
         100
     );
@@ -88,39 +97,55 @@ function test_getTime()
     for (params of [
         {
             date: new Date(0, 0, 0, 0, 0, 0),
-            expected12: "12:00:00 AM",
-            expected24: "00:00:00",
+            expected12: "12:00 AM",
+            expected24: "00:00",
+            expected12s: "12:00:00 AM",
+            expected24s: "00:00:00",
         },
         {
             date: new Date(0, 0, 0, 1, 0, 0),
-            expected12: "01:00:00 AM",
-            expected24: "01:00:00",
+            expected12: "01:00 AM",
+            expected24: "01:00",
+            expected12s: "01:00:00 AM",
+            expected24s: "01:00:00",
         },
         {
             date: new Date(0, 0, 0, 11, 0, 0),
-            expected12: "11:00:00 AM",
-            expected24: "11:00:00",
+            expected12: "11:00 AM",
+            expected24: "11:00",
+            expected12s: "11:00:00 AM",
+            expected24s: "11:00:00",
         },
         {
             date: new Date(0, 0, 0, 12, 0, 0),
-            expected12: "12:00:00 PM",
-            expected24: "12:00:00",
+            expected12: "12:00 PM",
+            expected24: "12:00",
+            expected12s: "12:00:00 PM",
+            expected24s: "12:00:00",
         },
         {
             date: new Date(0, 0, 0, 13, 0, 0),
-            expected12: "01:00:00 PM",
-            expected24: "13:00:00",
+            expected12: "01:00 PM",
+            expected24: "13:00",
+            expected12s: "01:00:00 PM",
+            expected24s: "13:00:00",
         },
         {
             date: new Date(0, 0, 0, 23, 0, 0),
-            expected12: "11:00:00 PM",
-            expected24: "23:00:00",
+            expected12: "11:00 PM",
+            expected24: "23:00",
+            expected12s: "11:00:00 PM",
+            expected24s: "23:00:00",
         },
     ]) {
-        actual = getTime(params["date"], { hours: 12 });
+        actual = getTime(params["date"], { hours: 12, seconds: "false" });
         assert_equal(actual, params["expected12"]);;
-        actual = getTime(params["date"], { hours: 24 });
+        actual = getTime(params["date"], { hours: 24, seconds: "false" });
         assert_equal(actual, params["expected24"]);;
+        actual = getTime(params["date"], { hours: 12, seconds: "true"});
+        assert_equal(actual, params["expected12s"]);;
+        actual = getTime(params["date"], { hours: 24, seconds: "true" });
+        assert_equal(actual, params["expected24s"]);;
     }
 }
 
